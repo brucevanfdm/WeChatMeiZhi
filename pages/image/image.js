8beae2314e8a9a1df5b472c14dbc621dc672cdbe
brucevan <brucevanfdm@gmail.com>
2016-11-15 23:29:59 +0800

    修改程序
    下拉自动加载替换成官方组件，替换部分即将废弃的控件。

--- pages/image/image.js
+++ pages/image/image.js
@@ -1,4 +1,4 @@
-Page( {
+Page({
     data: {
         url: "",
         hidden: false,
@@ -8,38 +8,38 @@ Page( {
         loadingText: "加载中..."
     },
 
-    onLoad: function( options ) {
+    onLoad: function (options) {
         that = this;
-        if( options == null || options.url == null ) {
-            this.setData( { hidden: true, toastHidden: false });
+        if (options == null || options.url == null) {
+            this.setData({ hidden: true, toastHidden: false });
             return;
         }
 
-        this.setData( {
+        this.setData({
             hidden: true,
             toastHidden: true,
             url: options.url
         })
     },
     //Toast信息改变
-    onToastChanged: function( event ) {
-        this.setData( { toastHidden: true });
+    onToastChanged: function (event) {
+        this.setData({ toastHidden: true });
     },
     // 长按
-    onlongclick: function() {
-        this.setData( { modalHidden: false });
+    onlongclick: function () {
+        this.setData({ modalHidden: false });
     },
     // 保存
-    onSaveClick: function( event ) {
+    onSaveClick: function (event) {
         var mUrl = "";
-        if( event.currentTarget.dataset.url != null )
+        if (event.currentTarget.dataset.url != null)
             mUrl = event.currentTarget.dataset.url;
-        console.log( "download：" + mUrl );
-        saveImage( mUrl );
+        console.log("download：" + mUrl);
+        saveImage(mUrl);
     },
     // 取消
-    onCancelClick: function( event ) {
-        this.setData( { modalHidden: true });
+    onCancelClick: function (event) {
+        this.setData({ modalHidden: true });
     },
 });
 
@@ -47,34 +47,34 @@ var that;
 /**
  * 保存图片
  */
-function saveImage( mUrl ) {
-    that.setData( {
+function saveImage(mUrl) {
+    that.setData({
         hidden: false,
         toastHidden: true,
         modalHidden: true,
         loadingText: "下载中..."
     });
-    wx.downloadFile( {
+    wx.downloadFile({
         url: mUrl,
         type: 'image',
-        success: function( res ) {
-            console.log( "download success" );
-            that.setData( {
+        success: function (res) {
+            console.log("download success");
+            that.setData({
                 hidden: true,
                 toastHidden: false,
                 toastText: "恭喜你，图片保存成功"
             });
         },
-        fail: function( res ) {
-            console.log( "download fail" );
-            that.setData( {
+        fail: function (res) {
+            console.log("download fail");
+            that.setData({
                 hidden: true,
                 toastHidden: false,
                 toastText: "保存失败，请稍后再试"
             });
         },
-        complete:function(res){
-            console.log( "download complete" );
+        complete: function (res) {
+            console.log("download complete");
         }
     })
 }
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,34 +1,38 @@
 //index.js
 //获取应用实例
 var app = getApp()
-Page( {
+Page({
   data: {
     items: [],
     hidden: false,
     loading: false,
-    loadmorehidden:true,
+    // loadmorehidden:true,
     plain: false
   },
 
-  onItemClick: function( event ) {
+  onItemClick: function (event) {
     var targetUrl = "/pages/image/image";
-    if( event.currentTarget.dataset.url != null )
+    if (event.currentTarget.dataset.url != null)
       targetUrl = targetUrl + "?url=" + event.currentTarget.dataset.url;
-    wx.navigateTo( {
+    wx.navigateTo({
       url: targetUrl
     });
   },
 
-  loadMore: function( event ) {
+  // loadMore: function( event ) {
+  //   var that = this
+  //   requestData( that, mCurrentPage + 1 );
+  // },
+
+  onReachBottom: function () {
+    console.log('onLoad')
     var that = this
-    requestData( that, mCurrentPage + 1 );
+    that.setData({
+      hidden: false,
+    });
+    requestData(that, mCurrentPage + 1);
   },
 
-  onLoad: function() {
-    console.log( 'onLoad' )
-    var that = this
-    requestData( that, mCurrentPage + 1 );
-  }
 })
 
 /**
@@ -50,38 +54,44 @@ var Constant = require('../../utils/constant.js');
  * @param that Page的对象，用来setData更新数据
  * @param targetPage 请求的目标页码
  */
-function requestData( that, targetPage ) {
-  wx.request( {
-    url: Constant.GET_MEIZHI_URL+targetPage,
+function requestData(that, targetPage) {
+  wx.showToast({
+    title: '加载中',
+    icon: 'loading'
+  });
+  wx.request({
+    url: Constant.GET_MEIZHI_URL + targetPage,
     header: {
       "Content-Type": "application/json"
     },
-    success: function( res ) {
-      if( res == null ||
+    success: function (res) {
+      if (res == null ||
         res.data == null ||
         res.data.results == null ||
-        res.data.results.length <= 0 ) {
+        res.data.results.length <= 0) {
 
-        console.error( "god bless you..." );
+        console.error("god bless you...");
         return;
       }
 
-      var i = 0;
-      for( ;i < res.data.results.length;i++ )
-        bindData( res.data.results[ i ] );
+
+      for (var i = 0; i < res.data.results.length; i++)
+        bindData(res.data.results[i]);
 
       //将获得的各种数据写入itemList，用于setData
       var itemList = [];
-      for( var i = 0;i < mUrl.length;i++ )
-        itemList.push( { url: mUrl[ i ], desc: mDesc[ i ], who: mWho[ i ], time: mTimes[ i ], title: mTitles[ i ] });
+      for (var i = 0; i < mUrl.length; i++)
+        itemList.push({ url: mUrl[i], desc: mDesc[i], who: mWho[i], time: mTimes[i], title: mTitles[i] });
 
-      that.setData( {
+      that.setData({
         items: itemList,
         hidden: true,
-        loadmorehidden:false,
+        // loadmorehidden:false,
       });
 
       mCurrentPage = targetPage;
+
+      wx.hideToast();
     }
   });
 }
@@ -90,16 +100,16 @@ function requestData( that, targetPage ) {
  * 绑定接口中返回的数据
  * @param itemData Gank.io返回的content;
  */
-function bindData( itemData ) {
+function bindData(itemData) {
 
-  var url = itemData.url.replace( "//ww", "//ws" );
+  var url = itemData.url.replace("//ww", "//ws");
   var desc = itemData.desc;
   var who = itemData.who;
-  var times = itemData.publishedAt.split( "T" )[ 0 ];
+  var times = itemData.publishedAt.split("T")[0];
 
-  mUrl.push( url );
-  mDesc.push( desc );
-  mWho.push( who );
-  mTimes.push( times );
-  mTitles.push( "publish by：" + "@" + who + " —— " + times );
+  mUrl.push(url);
+  mDesc.push(desc);
+  mWho.push(who);
+  mTimes.push(times);
+  mTitles.push("publish by：" + "@" + who + " —— " + times);
 }
